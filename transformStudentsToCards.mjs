db685395e363682a5644e2b11c3fbd2103fac562
denis dupont <denis.dupont@univ-evry.fr>
2024-11-12 07:07:50 +0100

    add container

--- transformStudentsToCards.mjs
+++ transformStudentsToCards.mjs
@@ -17,12 +17,12 @@ export const transformStudentsToCards = (students) => {
   // Transform students into Bootstrap card elements
   const studentsToHtml = students.map(student => {
     return `
-      <div class="card" data-cohorte="${student.cohorte}" style="width: 18rem;">
+      <div class="card" data-cohorte="${student.cohorte}">
         <img src="${student.img}" class="card-img-top" alt="${student.name}">
         <div class="card-body">
           <h5 class="card-title">${student.name}</h5>
           <p class="card-text">Mark: ${student.mark}</p>
-          <p class="card-text">Cohorte: ${student.cohorte}</p>
+          <!-- <p class="card-text">Cohorte: ${student.cohorte}</p> -->
         </div>
       </div>
     `;
